e3ffb167e3b801ae4725477152831c6af2492527
camilo franco <camilofinanzas@gmail.com>
2023-09-02 12:09:57 -0500

    primera version piedra papel tijeras

---
+++ app.py
@@ -0,0 +1,50 @@
+import random
+import streamlit as st
+
+
+presentacion = '''
+
+***************JUEGO DE NICO 👨‍💻********************
+*********Piedra Papel Tijeras***********
+         🧻  🪨   ✂️
+
+'''
+
+st.write(presentacion)
+#user_option = input('Por favor elija Piedra, papel o tijeras : ')
+st.header('Primera App de Curso de python : ')
+
+options = ['piedra','papel','tijeras']
+
+user_option = st.selectbox('Por favor elija una opcion', options)
+
+random_options = (random.randint(0,2))
+computer_option = options[random_options]
+# st.write(computer_option)
+
+
+
+st.write(f"Eleccion usuario 👨‍🦰: {user_option}")
+st.write(f"Eleccion computador 🤖: {computer_option}")
+
+
+if user_option.lower() == computer_option:
+    st.write("!!Empate!! ")
+
+elif user_option == "piedra":
+    if computer_option=="tijeras":
+        st.write("Usuario Gana Piedra rompe tijeras")
+    else :
+        st.write("Computador Gana papel tapa a piedra ☹️")
+
+elif user_option == "papel":
+    if computer_option == "tijeras":
+        st.write("Computador gana tijeras corta a papel☹️")
+    else :
+        st.write("Usuario Gana papel tapa a piedra") 
+
+elif user_option == "tijeras":
+    if computer_option == "piedra":
+        st.write("Computador gana piedra rompe tijeras ☹️")
+    else :
+        st.write("Usuario Gana tijeras corta papel")
